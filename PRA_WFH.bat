6cc333a3bb3a960cb08bf5ba18e1c21e81218266
BruceBanner113 <62618819+BruceBanner113@users.noreply.github.com>
2020-03-24 15:15:27 -0400

    Delete PRA_WFH.bat

--- PRA_WFH.bat
+++
@@ -1,4 +0,0 @@
-MKDIR C:\users\%username%\desktop\PRA_WFH
-bitsadmin /Transfer Workspace /download https://downloads.citrix.com/16907/CitrixWorkspaceApp.exe?__gda__=1585023615_784ff8674065e74ded70070c8f61a06f C:\users\%username%\desktop\PRA_WFH\CitrixWorkspaceApp.exe
-C:\users\%username%\desktop\PRA_WFH\CitrixWorkspaceApp.exe /silent
-Xcopy "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Citrix Workspace.lnk" C:\users\%username%\desktop /y
